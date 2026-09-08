2c400c806977fb1e102493d007f6d0dbe7ab58c1
Joy Deb Nath <connectwidjoy@gmail.com>
2026-09-08 21:14:45 +0600

    Fix stop_server.bat to kill pythonw.exe autostart instance

--- stop_server.bat
+++ stop_server.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Stop YT-Personal-Saver background server
 schtasks /end /tn "YT-Personal-Saver" >nul 2>&1
-powershell -NoProfile -Command "Get-CimInstance Win32_Process -Filter \"Name='python.exe'\" | Where-Object { $_.CommandLine -like '*server.py*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force; Write-Host \"Stopped PID $($_.ProcessId)\" }"
+powershell -NoProfile -Command "Get-CimInstance Win32_Process | Where-Object { ($_.Name -eq 'python.exe' -or $_.Name -eq 'pythonw.exe') -and $_.CommandLine -like '*server.py*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force; Write-Host ('Stopped PID ' + $_.ProcessId) }"
 echo Done. Server stopped.
 pause
